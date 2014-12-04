eebf38840d2e989624ff95efc06a63b6d66916f4
Sean Houghton <sean_houghton@playstation.sony.com>
2014-12-04 14:39:55 -0800

    More dictionary tries

--- 03-Dictionaries/01-defaults.py
+++ 03-Dictionaries/01-defaults.py
@@ -1,19 +1,30 @@
+import random
 
-def test_and_set(data):
-    if 'foo' in data:
-        value = data['foo']
+def test_and_set(data, key):
+    if key in data:
+        value = data[key]
     else:
         value = ''
     return value
 
 
-def default_value(data):
-    return data.get('foo', '')
+def try_except(data, key):
+    try:
+        return data[key]
+    except KeyError:
+        return ''
+
+
+def default_value(data, key):
+    return data.get(key, '')
 
 
 if __name__ == "__main__":
     data = {'foo': 10, 'bar': 20, 'jar': 30}
+    keys = data.keys() + ['missing']
     for x in range(0, 100000):
-        test_and_set(data)
+        test_and_set(data, random.choice(keys))
     for x in range(0, 100000):
-        default_value(data)
+        try_except(data, random.choice(keys))
+    for x in range(0, 100000):
+        default_value(data, random.choice(keys))
